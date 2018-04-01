1d37e068238788be6641f5b630f8de570854cb6c
anonym <anonym>
2018-04-01 16:05:23 +0200

    Included and linked GLEW, GLFW. Testcode for window.

--- Wolfenfels/main.cpp
+++ Wolfenfels/main.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 using namespace std;
 
 int main()
 {
-	cout << "Enter key to quit." << endl;
-	char c;
-	cin >> c;
-    return 0;
+	GLFWwindow* window;
+
+	// Initialize the library
+	if (!glfwInit() || !glewInit())
+		return -1;
+
+	// Create a windowed mode window and its OpenGL context
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	// Make the window's context current
+	glfwMakeContextCurrent(window);
+
+	// Loop until the user closes the window
+	while (!glfwWindowShouldClose(window))
+	{
+		// Render here
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Swap front and back buffers
+		glfwSwapBuffers(window);
+
+		// Poll for and process events
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 }
